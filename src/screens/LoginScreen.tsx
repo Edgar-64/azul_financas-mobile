a97522b38c0c2b0c524839380a71b151aa82d13b
Edgar-64 <edgarmaia.dev@gmail.com>
2026-05-14 21:58:16 -0300

    Update LoginScreen.tsx

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   SafeAreaView,
   Image,
+  Alert,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from '@react-native-async-storage/async-storage';
